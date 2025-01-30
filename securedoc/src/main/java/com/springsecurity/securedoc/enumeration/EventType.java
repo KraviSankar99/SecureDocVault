64585513537c938a912e55a7e41bc11efec7fe06
KraviSankar99 <ravisankar231299@gmail.com>
2025-01-30 12:08:01 -0600

    Delete securedoc directory

--- securedoc/src/main/java/com/springsecurity/securedoc/enumeration/EventType.java
+++
@@ -1,9 +0,0 @@
-package com.springsecurity.securedoc.enumeration;
-
-//
- /// 1/25/24
-//
-
-public enum EventType {
-    REGISTRATION, RESETPASSWORD
-}
--- securedoc/src/main/java/com/springsecurity/securedoc/enumeration/LoginType.java
+++
@@ -1,9 +0,0 @@
-package com.springsecurity.securedoc.enumeration;
-
-//
- /// 2/28/24
-//
-
-public enum LoginType {
-    LOGIN_ATTEMPT, LOGIN_SUCCESS
-}
